07cb662899c5347905cced7740475c88c25831b2
Mehreenaziz <113247845+Mehreenaziz@users.noreply.github.com>
2022-09-30 14:40:00 +0100

    imgc2

--- script.js
+++ script.js
@@ -4,24 +4,23 @@
   You can make this window bigger. 
    ===================
 */
+var piccies;
 
-var pics;
 
-
-pics = ['https://image.shutterstock.com/image-photo/behrai-kabab1-600w-154637972.jpg', 'https://image.shutterstock.com/image-photo/chicken-biryani-showing-leg-piece-600w-353098577.jpg', 'https://image.shutterstock.com/image-photo/samosa-on-plate-sauce-tomatoes-600w-255644446.jpg', 'https://image.shutterstock.com/image-photo/closeup-mince-paratha-600w-84751744.jpg', 'https://image.shutterstock.com/image-photo/tasty-butter-chicken-curry-dish-600w-1153329448.jpg', 'https://image.shutterstock.com/image-photo/hot-spicy-soup-chickpeas-onions-600w-1869258109.jpg'];
+piccies = ['https://image.shutterstock.com/image-photo/behrai-kabab1-600w-154637972.jpg', 'https://image.shutterstock.com/image-photo/chicken-biryani-showing-leg-piece-600w-353098577.jpg', 'https://image.shutterstock.com/image-photo/samosa-on-plate-sauce-tomatoes-600w-255644446.jpg', 'https://image.shutterstock.com/image-photo/closeup-mince-paratha-600w-84751744.jpg', 'https://image.shutterstock.com/image-photo/tasty-butter-chicken-curry-dish-600w-1153329448.jpg', 'https://image.shutterstock.com/image-photo/hot-spicy-soup-chickpeas-onions-600w-1869258109.jpg'];
 let element_carousel = document.getElementById('carousel');
 element_carousel.setAttribute("src", 'https://image.shutterstock.com/image-photo/behrai-kabab1-600w-154637972.jpg');
 
 
-document.getElementById('next').addEventListener('click', (event) => {
+document.getElementById('nex').addEventListener('click', (event) => {
   let element_carousel2 = document.getElementById('carousel');
-  element_carousel2.setAttribute("src", pics[0]);
+  element_carousel2.setAttribute("src", piccies[0]);
 
 });
 
-document.getElementById('previous').addEventListener('click', (event) => {
+document.getElementById('pre').addEventListener('click', (event) => {
   let element_carousel3 = document.getElementById('carousel');
-  element_carousel3.setAttribute("src", pics.slice(-1)[0]);
+  element_carousel3.setAttribute("src", piccies.slice(-1)[0]);
 
 });
 
